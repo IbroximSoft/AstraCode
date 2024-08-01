92672653e0cf748016d3e3bd38ad003d4e182493
Git <halilshax01@gmail.com>
2024-08-01 17:57:19 +0500

    Initial Commit

---
+++ AstraCode/AstraCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstraCodeApp.swift
+//  AstraCode
+//
+//  Created by Ibrohim - Soft on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct AstraCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
