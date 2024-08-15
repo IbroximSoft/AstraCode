18574d0ab79424f825e1c99205fab7b83d76a2e7
Git <halilshax01@gmail.com>
2024-08-15 22:20:29 +0500

    [UPD] Offline dialog

--- AstraCode/ContentView.swift
+++ AstraCode/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var network = Network()
+    
+    func reload() {
+       _ = network.checkConnection()
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+             
+            if (true) {
+                VStack{
+                    WebView(url: URL(string: "https://app.boshqar.com/")!)
+                }
+            }
+            else {
+                VStack {
+                    Text("No internet connection")
+                        .font(.largeTitle)
+                        .padding()
+                        .padding()
+                        .multilineTextAlignment(.center)
+                    Button("Reload") {
+                        reload()
+                    }
+                    .padding()
+                }
+            }
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
--- AstraCode/WebView.swift
+++ AstraCode/WebView.swift
@@ -2,7 +2,29 @@
 //  WebView.swift
 //  AstraCode
 //
-//  Created by Ibrohim - Soft on 04/08/24.
+//  Created by Ibrohim - Soft on 14/08/24.
 //
 
-import Foundation
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+
+    var url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+}
+  
+extension WKWebView {
+    override open var safeAreaInsets: UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+}
+
